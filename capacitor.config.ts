f9e950572dd53c1132be1e3881ea5fc5ea30cfcb
Ashish kumar shah <ashish.shah.with@gmail.com>
2026-07-21 01:38:47 +0530

    Convert capacitor config from TS to JSON to bypass typescript requirement

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.deardiary.app',
-  appName: 'Dear Diary',
-  webDir: 'dist'
-};
-
-export default config;
